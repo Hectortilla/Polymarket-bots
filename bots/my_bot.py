69cda753d9c7ec9a1f0c56ea8e920415f5e529d5
Hector Soria <hector.soria@with-madrid.com>
2026-07-11 15:36:47 +0200

    fix

--- bots/my_bot.py
+++ bots/my_bot.py
@@ -6,5 +6,4 @@ from examples.example_rebound import ExampleReboundBot
 def create(config):
     return ExampleReboundBot(
         os.environ.get("BOT_OUTCOME_LABEL", "Yes"),
-        market_slug=os.environ.get("BOT_MARKET_SLUG"),
     )
